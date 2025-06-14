cccb61b1c26a96d903f1814ec4ebf4def2478b2a
juarez-dev <101058689+juarez-dev@users.noreply.github.com>
2025-06-14 12:10:35 +0200

    Update translations.js

--- javaScript/translations.js
+++ javaScript/translations.js
@@ -3,14 +3,12 @@ document.addEventListener("DOMContentLoaded", function () {
     // Text definition in diferent languages
     const translations = {
         en: {
-            name: "Ismael Juarez Garcia",
             role: "Technician in Administration and Finance & App Developer",
             github: "My GitHub",
             contact: "Contact me:",
             writtenBy: "Written by",
         },
         es: {
-            name: "Ismael Juárez García",
             role: "Técnico en Administración y Finanzas & Desarrollador de Apps",
             github: "Mi GitHub",
             contact: "Contáctame:",
@@ -30,7 +28,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const lang = params.get("lang") || "en"; // Default: inglés
 
     // Aplicar traducciones
-    document.getElementById("name").textContent = translations[lang].name;
     document.getElementById("role").textContent = translations[lang].role;
     document.getElementById("github").textContent = translations[lang].github;
     document.getElementById("contact").textContent = translations[lang].contact;
